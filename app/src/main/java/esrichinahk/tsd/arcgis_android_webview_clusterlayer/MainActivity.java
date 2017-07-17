19e2d6e654d293e046970f48b78b2839f32bbf6f
Jack Huang <bbjackhuang3343@gmail.com>
2017-07-17 12:59:15 +0800

    Clean init commit

---
+++ app/src/main/java/esrichinahk/tsd/arcgis_android_webview_clusterlayer/MainActivity.java
@@ -0,0 +1,13 @@
+package esrichinahk.tsd.arcgis_android_webview_clusterlayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
