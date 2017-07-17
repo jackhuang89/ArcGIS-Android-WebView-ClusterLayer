3facc72d83e4246c27bb33c8ca1c8c706f7f6713
Jack Huang <bbjackhuang3343@gmail.com>
2017-07-17 14:40:22 +0800

    Commit the web view. Finished status.

--- app/src/main/java/esrichinahk/tsd/arcgis_android_webview_clusterlayer/MainActivity.java
+++ app/src/main/java/esrichinahk/tsd/arcgis_android_webview_clusterlayer/MainActivity.java
@@ -1,7 +1,10 @@
 package esrichinahk.tsd.arcgis_android_webview_clusterlayer;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        myWebView.loadUrl("http://10.0.2.2/cl/styled.html");
     }
 }
